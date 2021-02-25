5b4b71e58cb15a08b29ba06cd2ccfc4078344b2d
Manveer Singh <66374399+Mgrewal2000@users.noreply.github.com>
2021-02-25 13:21:13 -0500

    Comment blocks push

--- src/pkg/Company.java
+++ src/pkg/Company.java
@@ -19,8 +19,10 @@ public class Company {
     }
 
     /**
+     * getter method that returns the private instance variable numEmployees of
+     * class Company when called in other classes
      * 
-     * @return
+     * @return returns the instance variable numEmployee in this class
      */
     public int getNumEmployees() {
         return this.numEmployees;
@@ -28,7 +30,7 @@ public class Company {
 
     /**
      * Returns the index of the employee that we are trying to find in the empList
-     * array of type Employee acts as an helper method for other methods to identy
+     * array of type Employee acts as an helper method for other methods to identify
      * certain employees
      * 
      * @param employee
@@ -46,9 +48,9 @@ public class Company {
     }
 
     /**
-     * Automatically grows the capacity of array to hold employees by 4 using the
-     * method grow() below. Creating a new instance of type Employee and growing the
-     * capacity by 4.
+     * Grows the capacity of array to hold employees by 4 using the method grow()
+     * below. Creating a new instance of type Employee and growing the capacity by
+     * 4.
      */
     private void grow() {
         Employee[] newList = new Employee[emplist.length + 4];
@@ -59,7 +61,10 @@ public class Company {
     }
 
     /**
-     * removes a certain employee from the array empList based on user input
+     * checks the profile of an employee before adding them. Calls the find() method
+     * in this class, if employee already exists return false, otherwise checks to
+     * see if private instance variable numEmployees is equal to the length of
+     * emplist,if it does grow.
      * 
      * @param employee object of class Employee.
      * @return returns true if employee added successfully, false if employee is
@@ -82,7 +87,8 @@ public class Company {
     }
 
     /**
-     * removes a certain employee from the array empList based on user input
+     * removes a certain employee from the array empList based on user input. calls
+     * the find() method in this class, if does not exist, return false,
      * 
      * @param employee object of class Employee.
      * @return returns true if removed successfully, false if employee cannot be
@@ -130,8 +136,10 @@ public class Company {
     }
 
     /**
-     * 
-     * 
+     * iterates through the array emplist and processes payments for all employees
+     * based on their index. As long as the emplist[i] is satisfied and not null,
+     * calculatePayment() is called on every employee in the array emplist of type
+     * Employee
      */
     public void processPayments() {
         for (int i = 0; i < numEmployees; i++) {
@@ -143,7 +151,7 @@ public class Company {
 
     /**
      * prints the contents (last name, firstname, department, earnings) of emplist
-     * array of type Employee using the .toString() method
+     * array of type Employee using the .toString() method.
      */
     public void print() {
         if (numEmployees == 0) {
@@ -159,7 +167,12 @@ public class Company {
     }
 
     /**
-     * 
+     * this method prints earning statements filtered by their respective
+     * department. Using a simple for loop and a key set to index i of our emplist
+     * array, we are able to compare the profile and the department using their
+     * respective getter methods in a while loop. Finally iterating over emplist
+     * again, as long as emplist[i] is not = null, we use the .toString method in
+     * employee class to print the earnings statements
      */
     public void printByDepartment() {
         if (numEmployees == 0) {
@@ -187,7 +200,11 @@ public class Company {
     }
 
     /**
-     * 
+     * this method prints earning statements filtered by their respective hired
+     * date. Base case of numEmployees equaling 0 is taken care of in the first if
+     * statement then using the same approach as in printByDepartment, we are able
+     * to use a key of type Employee to compare the profile and dateHired in the
+     * while loop and then another for loop prints out the earnings
      */
     public void printByDate() {
         if (numEmployees == 0) {
--- src/pkg/Date.java
+++ src/pkg/Date.java
@@ -13,7 +13,15 @@ public class Date implements Comparable<Date> {
     private int year;
 
     /**
+     * constructor that allows us to split the date into months, days, and year
+     * using the .split() and passing in our delimiter "/" token[0] is the
+     * month...for example (01/02/2020.. 01 is extracted) token[1] is set to the
+     * date passed. (01/02/2020..02 is extracted) token 2 is set to year. For
+     * example (01/02/2020...2020 is extractd)
      * 
+     * @param date passed in by the user which later gets split into month, day and
+     *             year. Initializes the instance variables month, day,year to the
+     *             tokens of the String date broken up
      */
     public Date(String date) { // Creates date object from input String
         String[] tokens = date.split("/");
@@ -23,39 +31,58 @@ public class Date implements Comparable<Date> {
     }
 
     /**
-     * 
+     * Another constructor that returns today's date using the getInstance() method.
+     * sets the private instance variables year, month and day to instances of
+     * calendar
      */
     public Date() { // return today’s date
         Calendar calendar = Calendar.getInstance();
 
-        year = calendar.get(calendar.YEAR);
-        month = calendar.get(calendar.MONTH) + 1;
-        day = calendar.get(Calendar.DAY_OF_MONTH);
+        this.year = calendar.get(calendar.YEAR);
+        this.month = calendar.get(calendar.MONTH) + 1;
+        this.day = calendar.get(Calendar.DAY_OF_MONTH);
     }
 
     /**
+     * getter method that returns the private instance variable year of class Date
      * 
+     * @return year, from class Date
      */
     public int getYear() {
-        return year;
+        return this.year;
     }
 
     /**
+     * getter method that returns the private instance variable month of class Date
      * 
+     * @return month, from class Date
      */
     public int getMonth() {
-        return month;
+        return this.month;
     }
 
     /**
+     * getter method that retuns the private instance variable day of class Date
      * 
+     * @return day, from class date
      */
     public int getDay() {
-        return day;
+        return this.day;
     }
 
     /**
+     * method that compares the the object date of Date class (parameter) to the
+     * instance variables of this class and returns -1 if year of date object is
+     * greater than instance variable year, returns 1 it's less.
      * 
+     * Same for month and day, returns -1 if month/day is greater than their
+     * respective instance variables, 1 otherwise
+     * 
+     * @param date that gets passed in when called and used to compare the year,
+     *             month, and day
+     * @return -1 if year, month, day of date object is > than their respective
+     *         instance variables. 1 if year,month, day of date object is < than
+     *         their respective instance variables, 0 otherwise.
      */
     @Override
     public int compareTo(Date date) { // return 1, 0, or -1
@@ -81,7 +108,13 @@ public class Date implements Comparable<Date> {
     }
 
     /**
+     * method that checks to see if a date is valid. Base case of a year less than
+     * 1900 or greater than this year (2021) is handled by the if statement followed
+     * up with all the rules as mentioned in project 1 description. Date must be >=
+     * 1900 and less than <= 2021. Specific months have ceratin amount of days,
+     * february having 29 days in a leap year is also handled.
      * 
+     * @return true if the date is valid based on satisfaction, false otherwise.
      */
     public boolean isValid() {
         if (year < 1900 || year > 2021) {
@@ -140,27 +173,27 @@ public class Date implements Comparable<Date> {
     }
 
     @Override
-    public boolean equals(Object obj){
+    public boolean equals(Object obj) {
         Date other;
-        try{
+        try {
             other = (Date) obj;
-        }catch(Exception e){
+        } catch (Exception e) {
             return false;
         }
 
-        if(this.compareTo(other) == 0){
+        if (this.compareTo(other) == 0) {
             return true;
         }
         return false;
     }
 
     @Override
-    public String toString(){
+    public String toString() {
 
         String mm = String.valueOf(month);
         String dd = String.valueOf(day);
         String yyyy = String.valueOf(year);
-        
+
         return mm + "/" + dd + "/" + yyyy;
     }
 
