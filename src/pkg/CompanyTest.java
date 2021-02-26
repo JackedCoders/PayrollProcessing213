c10b8f499b0f26ef72586a3b32c0953c7068b255
Manveer Singh <66374399+Mgrewal2000@users.noreply.github.com>
2021-02-25 22:44:56 -0500

    tests

--- src/pkg/CompanyTest.java
+++ src/pkg/CompanyTest.java
@@ -7,7 +7,13 @@ package pkg;
 class CompanyTest {
 
     public void testAdd() {
+        Company comp1 = new Company();
+        Date date1 = new Date("01/12/2020");
+        Profile newPerson = new Profile("John, Doe", "CS", date1);
 
+        Employee parttime = new Parttime(newPerson, 24.3);
+
+        assertTrue(comp1.add(parttime));
     }
 
     public void testRemove() {
