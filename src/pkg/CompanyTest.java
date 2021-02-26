739d94b8fc0ce1fa22c3eecba795852314b86a77
Manveer Singh <66374399+Mgrewal2000@users.noreply.github.com>
2021-02-25 21:55:58 -0500

    test

--- src/pkg/CompanyTest.java
+++ src/pkg/CompanyTest.java
@@ -1,4 +1,12 @@
+/**
+ * Class that thoroughly tests Company class( add(), remove(), setHours() methods)
+ * @author Manveer Singh, Prasish Sriram
+ */
 package pkg;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junir.jupiter.api.Assertions.assertFalse;
 
 public class CompanyTest {
 
