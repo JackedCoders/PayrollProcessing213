7cf2fa4db4a292e2421b2fa84f4742648c7068bd
Manveer Singh <66374399+Mgrewal2000@users.noreply.github.com>
2021-02-25 22:10:45 -0500

    test

---
+++ src/pkg/CompanyTest.java
@@ -0,0 +1,18 @@
+package pkg;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CompanyTest {
+
+    @org.junit.jupiter.api.Test
+    void add() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setHours() {
+    }
+}
